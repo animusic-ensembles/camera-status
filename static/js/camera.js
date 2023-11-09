1471451f54a119582ac5e096eed466cd7cb8869b
Eric Shim <rcshim04@gmail.com>
2023-11-08 21:53:03 -0500

    first commit

---
+++ static/js/camera.js
@@ -0,0 +1,66 @@
+const API_URL = "http://localhost:5000/cameras";
+
+$(document).ready(function() {
+    const name = window.location.pathname.split("/")[1];
+    const key = new URLSearchParams(window.location.search).get("key");
+
+    $(document).on("click tap", ".active", async function() {
+        const response = await fetch(`${API_URL}?key=${key}`);
+        const data = await response.json();
+        if(data.error) {
+            alert("No Key Provided");
+        }
+        data.forEach(async (camera) => {
+            if(camera.name != name) return;
+            if($(this).attr("id") == "online") {
+                camera.online = !camera.online;
+            } else if($(this).attr("id") == "ready") {
+                camera.ready = !camera.ready;
+            }
+        });
+        await fetch(`${API_URL}?key=${key}`, {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+    });
+
+    setInterval(async function() {
+        const response = await fetch(`${API_URL}?key=${key}`);
+        const data = await response.json();
+        console.log(data);
+        if(data.error) {
+            alert("No Key Provided");
+        }
+        let current = "N/A";
+        data.forEach((camera) => {
+            let status = "Offline";
+            if(camera.online) {
+                status = "Online";
+                if(camera.ready) status = "Ready";
+                if(camera.live) {
+                    status = "Live";
+                    current = camera.name;
+                }
+            };
+            if(camera.name != name) return;
+            if(!camera.online) {
+                $("#ready").removeClass("active").addClass("inactive");
+            } else {
+                $("#ready").removeClass("inactive").addClass("active");
+            }
+            if(camera.ready) {
+                $("#online").removeClass("active").addClass("inactive");
+            } else {
+                $("#online").removeClass("inactive").addClass("active");
+            }
+            if(camera.live) {
+                $("#online").removeClass("active").addClass("inactive");
+                $("#ready").removeClass("active").addClass("inactive");
+            }
+            $("#online").html(camera.online ? "Go Offline" : "Go Online");
+            $("#ready").html(camera.ready ? "Not Ready" : "Ready");
+            $(`.status`).html(status).removeClass("offline online ready live").addClass(status.toLowerCase());
+        });
+        $(".current").html(current);
+    }, 200);
+});
---
+++ static/js/cameras.js
@@ -0,0 +1,75 @@
+const API_URL = "http://localhost:5000/cameras";
+
+$(document).ready(function() {
+    const key = new URLSearchParams(window.location.search).get("key");
+
+    $(".clear").on("click tap", async function() {
+        const response = await fetch(`${API_URL}?key=${key}`);
+        const data = await response.json();
+        if(data.error) {
+            alert("No Key Provided");
+        }
+        data.forEach(async (camera) => {
+            camera.live = false;
+        });
+        await fetch(`${API_URL}?key=${key}`, {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+        $(".current").html("N/A");
+    });
+
+    $(document).on("click tap", ".select", async function() {
+        const response = await fetch(`${API_URL}?key=${key}`);
+        const data = await response.json();
+        if(data.error) {
+            alert("No Key Provided");
+        }
+        data.forEach(async (camera) => {
+            if(camera.name == $(this).attr("id")) {
+                camera.live = !camera.live;
+            } else {
+                camera.live = false;
+            }
+        });
+        await fetch(`${API_URL}?key=${key}`, {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+    });
+
+    setInterval(async function() {
+        const response = await fetch(`${API_URL}?key=${key}`);
+        const data = await response.json();
+        console.log(data);
+        if(data.error) {
+            alert("No Key Provided");
+        }
+        let current = "N/A";
+        data.forEach((camera) => {
+            $(`#${camera.name} .icon`).html(camera.online ? "photo_camera" : "no_photography");
+            let state = "inactive";
+            let status = "Offline";
+
+            if(camera.online) {
+                status = "Online";
+                state = "active";
+                if(camera.ready) {
+                    status = "Ready";
+                    state = "select";
+                }
+                if(camera.live) {
+                    status = "Live";
+                    state = "select";
+                    current = camera.name;
+                }
+            };
+            if(status == "Live") $(`#${camera.name}`).addClass("live");
+            else $(`#${camera.name}`).removeClass("live");
+            $(`#${camera.name}`).removeClass("active inactive select").addClass(state);
+            $(`#${camera.name} .icon`).html(camera.online ? "photo_camera" : "no_photography");
+            $(`#${camera.name} .status`).html(status);
+        });
+        $(".current").html(current);
+    }, 200);
+});
